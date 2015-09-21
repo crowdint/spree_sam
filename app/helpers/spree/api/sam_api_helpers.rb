ba2e3fb025a5dcbfeb3070818f758f458f4292af
Edwin Cruz <softr8@gmail.com>
2015-09-21 18:39:01 -0500

    Adding calculator attribute mapping hash

--- app/helpers/spree/api/sam_api_helpers.rb
+++ app/helpers/spree/api/sam_api_helpers.rb
@@ -2,36 +2,38 @@ module Spree
   module Api
     module SamApiHelpers
 
-  SAM_ATTRIBUTES = [
-      :promotion_attributes,
-      :promotion_action_attributes,
-      :promotion_rule_attributes
-  ]
+      SAM_ATTRIBUTES = [
+          :promotion_attributes,
+          :promotion_action_attributes,
+          :promotion_rule_attributes
+      ]
 
-  mattr_reader *SAM_ATTRIBUTES
+      mattr_reader *SAM_ATTRIBUTES
 
-  @@promotion_attributes = [
-      :id, :name, :type, :usage_limit, :code, :path, :expires_at, :starts_at,
-      :description, :advertise, :match_policy
-  ]
+      @@promotion_attributes = [
+          :id, :name, :type, :usage_limit, :code, :path, :expires_at, :starts_at,
+          :description, :advertise, :match_policy
+      ]
 
-  @@promotion_action_attributes = [
-      :id, :type, :position
-  ]
+      @@promotion_action_attributes = [
+          :id, :type, :position
+      ]
 
-  @@promotion_rule_attributes = [
-      :id, :type, :code
-  ]
+      @@promotion_rule_attributes = [
+          :id, :type, :code
+      ]
 
-  def calculator_attribute calculator
-    mapping = {
-        'FlatRate' => 'preferred_amount',
-        'FlatPercentItemTotal' => 'preferred_flat_percent',
-    }
-    klass = calculator.type.demodulize
-    {
-        mapping[klass] => calculator.send(mapping[klass])
-    }
+      def calculator_attribute calculator
+        mapping = {
+            'FlatRate' => 'preferred_amount',
+            'FlatPercentItemTotal' => 'preferred_flat_percent',
+        }
+        klass = calculator.type.demodulize
+        {
+            mapping[klass] => calculator.send(mapping[klass])
+        }
+      end
+
+    end
   end
-
 end
